6e5a9138168793c167d0b769a5ed074bc3a49310
최수원 <suwon3274@gmail.com>
2023-05-31 23:33:44 +0900

    :fire: :: Deprecated 코드 삭제

--- Rolling-Application/src/main/java/com/stubee/rollingapplication/domain/member/port/api/QueryMemberUseCase.java
+++ Rolling-Application/src/main/java/com/stubee/rollingapplication/domain/member/port/api/QueryMemberUseCase.java
@@ -1,6 +1,5 @@
 package com.stubee.rollingapplication.domain.member.port.api;
 
-import com.stubee.rollingcore.domain.member.dto.response.MemberInfoResponse;
 import com.stubee.rollingcore.domain.member.model.Member;
 
 import java.util.UUID;
@@ -10,17 +9,5 @@ public interface QueryMemberUseCase {
     Member getMy();
 
     Member getMemberById(UUID memberId);
-
-    /**
-     * @deprecated 내 정보 api 분리로 인해 삭제 에정
-     * */
-    @Deprecated
-    MemberInfoResponse getMyInfo();
-
-    /**
-     * @deprecated 내 정보 api 분리로 인해 삭제 에정
-     * */
-    @Deprecated
-    MemberInfoResponse getInfo(UUID memberId);
 
 }
