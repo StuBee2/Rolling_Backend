8ba8e6f238032822dfb04b7ad032c2fd7ac70805
최수원 <suwon3274@gmail.com>
2023-05-31 23:51:23 +0900

    :fire: :: Deprecated 코드 삭제

--- Rolling-Adapter/src/main/java/com/stubee/rollingadapter/in/web/member/QueryMemberController.java
+++ Rolling-Adapter/src/main/java/com/stubee/rollingadapter/in/web/member/QueryMemberController.java
@@ -1,6 +1,5 @@
 package com.stubee.rollingadapter.in.web.member;
 
-import com.stubee.rollingcore.domain.member.dto.response.MemberInfoResponse;
 import com.stubee.rollingapplication.domain.member.port.api.QueryMemberUseCase;
 import com.stubee.rollingcore.domain.member.model.Member;
 import io.swagger.v3.oas.annotations.Operation;
@@ -31,28 +30,6 @@ public class QueryMemberController {
     @GetMapping("/{id}")
     public Member getMember(@PathVariable("id") UUID memberId) {
         return queryMemberUseCase.getMemberById(memberId);
-    }
-
-    /**
-     * @deprecated 내 정보 api 분리로 인해 삭제 에정
-     * */
-    @Deprecated
-    @Operation(description = "내 정보 조회")
-    @GetMapping("/my")
-    @ResponseStatus(OK)
-    public MemberInfoResponse getMyInfo() {
-        return queryMemberUseCase.getMyInfo();
-    }
-
-    /**
-     * @deprecated 내 정보 api 분리로 인해 삭제 에정
-     * */
-    @Deprecated
-    @Operation(description = "id로 Member 정보 조회")
-    @GetMapping("/info/{id}")
-    @ResponseStatus(OK)
-    public MemberInfoResponse getInfo(final @PathVariable("id") UUID memberId) {
-        return queryMemberUseCase.getInfo(memberId);
     }
 
 }
