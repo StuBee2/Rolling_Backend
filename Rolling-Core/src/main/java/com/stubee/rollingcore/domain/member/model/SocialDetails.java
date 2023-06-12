2216b745761e51b85dd2d4612507f8fb76746365
최수원 <suwon3274@gmail.com>
2023-06-12 10:53:47 +0900

    :memo: :: commit 누락 추가

--- Rolling-Core/src/main/java/com/stubee/rollingcore/domain/member/model/SocialDetails.java
+++ Rolling-Core/src/main/java/com/stubee/rollingcore/domain/member/model/SocialDetails.java
@@ -1,12 +1,34 @@
 package com.stubee.rollingcore.domain.member.model;
 
 import com.stubee.rollingcore.domain.member.enums.LoginType;
+import lombok.AccessLevel;
 import lombok.Builder;
 
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
 public record SocialDetails(
         String socialId,
         LoginType loginType,
         String name,
         String email,
-        String imageUrl) {}
+        String imageUrl) {
+    public static SocialDetails create(final String socialId, final LoginType loginType, final String name,
+                                       final String email, final String imageUrl) {
+        return SocialDetails.builder()
+                .socialId(socialId)
+                .loginType(loginType)
+                .name(name)
+                .email(email)
+                .imageUrl(imageUrl)
+                .build();
+    }
+
+    public SocialDetails updateNameAndEmail(final String name, final String email) {
+        return SocialDetails.builder()
+                .socialId(socialId)
+                .loginType(loginType)
+                .name(name)
+                .email(email)
+                .imageUrl(imageUrl)
+                .build();
+    }
+}
