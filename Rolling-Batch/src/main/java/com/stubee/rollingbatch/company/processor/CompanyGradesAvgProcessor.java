af246d57d3d9ef1f583a5e304b7e14ed4b2ab005
최수원 <suwon3274@gmail.com>
2023-08-03 01:27:54 +0900

    :memo: :: Company Grades update 로직 수정

--- Rolling-Batch/src/main/java/com/stubee/rollingbatch/company/processor/CompanyGradesAvgProcessor.java
+++ Rolling-Batch/src/main/java/com/stubee/rollingbatch/company/processor/CompanyGradesAvgProcessor.java
@@ -3,6 +3,7 @@ package com.stubee.rollingbatch.company.processor;
 import com.stubee.rollingapplication.domain.review.port.api.query.QueryReviewInfoListByCompanyUseCase;
 import com.stubee.rollingbatch.common.annotation.Processor;
 import com.stubee.rollingcore.common.dto.request.PageRequest;
+import com.stubee.rollingcore.common.model.Grades;
 import com.stubee.rollingcore.domain.company.model.Company;
 import com.stubee.rollingcore.domain.review.response.ReviewInfoResponse;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +22,6 @@ public class CompanyGradesAvgProcessor implements ItemProcessor<List<Company>, L
 
     private final QueryReviewInfoListByCompanyUseCase queryReviewInfoListByCompanyUseCase;
 
-    private double totalSum;
     private double salaryAndBenefitsSum;
     private double workLifeBalanceSum;
     private double organizationalCultureSum;
@@ -35,6 +35,8 @@ public class CompanyGradesAvgProcessor implements ItemProcessor<List<Company>, L
         final List<Company> processedCompanyList = new ArrayList<>();
 
         for (Company company : readCompanyList) {
+            log.info("Company Name : {}", company.companyDetails().name());
+
             long reviewPage = 1, reviewCnt = 0;
             this.initSums();
 
@@ -52,21 +54,21 @@ public class CompanyGradesAvgProcessor implements ItemProcessor<List<Company>, L
                 }
             }
 
-            final double totalAvg = calculateAvg(totalSum, reviewCnt);
             final double salaryAndBenefitsAvg = calculateAvg(salaryAndBenefitsSum, reviewCnt);
             final double workLifeBalanceAvg = calculateAvg(workLifeBalanceSum, reviewCnt);
             final double organizationalCultureAvg = calculateAvg(organizationalCultureSum, reviewCnt);
             final double careerAdvancementAvg = calculateAvg(careerAdvancementSum, reviewCnt);
 
-            Company updatedCompany = company.updateGrades(totalAvg, salaryAndBenefitsAvg, workLifeBalanceAvg,
+            Grades updatedGrades = Grades.create(salaryAndBenefitsAvg, workLifeBalanceAvg,
                     organizationalCultureAvg, careerAdvancementAvg);
 
-            log.info("totalAvg : {}", totalAvg);
+            Company updatedCompany = company.updateGrades(updatedGrades);
+
+            log.info("totalAvg : {}", updatedGrades.totalGrade());
             log.info("salaryAndBenefitsAvg : {}", salaryAndBenefitsAvg);
             log.info("workLifeBalanceAvg : {}", workLifeBalanceAvg);
             log.info("organizationalCultureAvg : {}", organizationalCultureAvg);
             log.info("careerAdvancementAvg : {}", careerAdvancementAvg);
-            log.info("Company Name : {}", updatedCompany.companyDetails().name());
 
             processedCompanyList.add(updatedCompany);
         }
@@ -77,7 +79,7 @@ public class CompanyGradesAvgProcessor implements ItemProcessor<List<Company>, L
     }
 
     private void initSums() {
-        totalSum = salaryAndBenefitsSum = workLifeBalanceSum = organizationalCultureSum = careerAdvancementSum = 0.0;
+        salaryAndBenefitsSum = workLifeBalanceSum = organizationalCultureSum = careerAdvancementSum = 0.0;
     }
 
     private PageRequest pageRequest(final long page) {
@@ -86,7 +88,6 @@ public class CompanyGradesAvgProcessor implements ItemProcessor<List<Company>, L
 
     private void calculateSum(final List<ReviewInfoResponse> reviewList) {
         for (ReviewInfoResponse response : reviewList) {
-            totalSum += response.totalGrade();
             salaryAndBenefitsSum += response.salaryAndBenefits();
             workLifeBalanceSum += response.workLifeBalance();
             organizationalCultureSum += response.organizationalCulture();
