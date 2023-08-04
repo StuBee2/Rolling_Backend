914d1c3da6039631c12825c65ef02bfedbf788cc
최수원 <suwon3274@gmail.com>
2023-08-05 00:16:20 +0900

    :memo: :: EmploymentJpaEntity -> EmploymentEntity

--- Rolling-Adapter/src/main/java/com/stubee/rollingadapter/persistence/employment/entity/EmploymentEntity.java
+++ Rolling-Adapter/src/main/java/com/stubee/rollingadapter/persistence/employment/entity/EmploymentEntity.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class EmploymentJpaEntity {
+public class EmploymentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- Rolling-Adapter/src/main/java/com/stubee/rollingadapter/persistence/employment/repository/EmploymentJpaRepository.java
+++ Rolling-Adapter/src/main/java/com/stubee/rollingadapter/persistence/employment/repository/EmploymentJpaRepository.java
@@ -1,9 +1,9 @@
 package com.stubee.rollingadapter.persistence.employment.repository;
 
-import com.stubee.rollingadapter.persistence.employment.entity.EmploymentJpaEntity;
+import com.stubee.rollingadapter.persistence.employment.entity.EmploymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmploymentJpaRepository extends JpaRepository<EmploymentJpaEntity, Long> {
+public interface EmploymentJpaRepository extends JpaRepository<EmploymentEntity, Long> {
 }
