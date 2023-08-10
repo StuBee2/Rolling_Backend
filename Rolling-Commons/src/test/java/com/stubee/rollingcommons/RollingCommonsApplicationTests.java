af7f10bdeda78b4780d0699adf38dbfca8b054ab
최수원 <suwon3274@gmail.com>
2023-08-11 06:03:49 +0900

    :fire: :: 기존 파일 삭제

--- Rolling-Commons/src/test/java/com/stubee/rollingcommons/RollingCommonsApplicationTests.java
+++ Rolling-Commons/src/test/java/com/stubee/rollingcommons/RollingCommonsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.stubee.rollingapplication;
+package com.stubee.rollingcommons;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RollingApplicationTests {
+class RollingCommonsApplicationTests {
 
     @Test
     void contextLoads() {
