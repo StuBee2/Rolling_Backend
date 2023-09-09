6141ad5a3d6d0d34ca3da937c284f6fe530bc65d
최수원 <suwon3274@gmail.com>
2023-09-10 01:52:14 +0900

    :art: :: MemberDomainService

--- Rolling-Application/member-application/src/main/java/com/stubee/memberapplication/usecases/impl/QueryMyInfoApi.java
+++ Rolling-Application/member-application/src/main/java/com/stubee/memberapplication/usecases/impl/QueryMyInfoApi.java
@@ -1,20 +1,20 @@
-package com.stubee.memberapplication.services.query;
+package com.stubee.memberapplication.usecases.impl;
 
 import com.stubee.applicationcommons.annotations.QueryService;
 import com.stubee.memberapplication.usecases.QueryMyInfoUseCase;
 import com.stubee.rollingdomains.domain.member.model.Member;
-import com.stubee.rollingdomains.domain.member.ports.LoadCurrentMemberPort;
+import com.stubee.rollingdomains.domain.member.services.GetMemberInfoService;
 import lombok.RequiredArgsConstructor;
 
 @QueryService
 @RequiredArgsConstructor
-public class QueryMyInfoService implements QueryMyInfoUseCase {
+public class QueryMyInfoApi implements QueryMyInfoUseCase {
 
-    private final LoadCurrentMemberPort loadCurrentMemberPort;
+    private final GetMemberInfoService queryMemberInfoService;
 
     @Override
     public Member get() {
-        return loadCurrentMemberPort.getMember();
+        return queryMemberInfoService.getMember();
     }
 
 }
