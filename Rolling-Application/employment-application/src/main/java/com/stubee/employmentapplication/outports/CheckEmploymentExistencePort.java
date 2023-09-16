d0d21c65b449f49daadd7ff1fcebc5d018955bd3
최수원 <suwon3274@gmail.com>
2023-09-16 21:34:44 +0900

    :sparkles: :: CHeckEmploymentExistenceService 추가

--- Rolling-Application/employment-application/src/main/java/com/stubee/employmentapplication/outports/CheckEmploymentExistencePort.java
+++ Rolling-Application/employment-application/src/main/java/com/stubee/employmentapplication/outports/CheckEmploymentExistencePort.java
@@ -1,17 +1,9 @@
 package com.stubee.employmentapplication.outports;
 
-import com.stubee.rollingdomains.domain.employment.exception.EmploymentExistException;
-
 import java.util.UUID;
 
 public interface CheckEmploymentExistencePort {
 
     boolean check(UUID employeeId, UUID employerId);
-
-    default void checkByEmployeeAndEmployer(final UUID employeeId, final UUID employerId) {
-        if(check(employeeId, employerId)) {
-            throw EmploymentExistException.EXCEPTION;
-        }
-    }
 
 }
