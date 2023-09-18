0059b067619ca2f012d23fe8f89630fbf69ebcf6
최수원 <suwon3274@gmail.com>
2023-09-18 23:11:11 +0900

    :art :: exception throw 위치 수정

--- Rolling-Application/member-application/src/main/java/com/stubee/memberapplication/services/MemberDomainService.java
+++ Rolling-Application/member-application/src/main/java/com/stubee/memberapplication/services/MemberDomainService.java
@@ -7,9 +7,10 @@ import com.stubee.memberapplication.outports.QueryMemberPort;
 import com.stubee.rollingdomains.domain.member.exception.DuplicatedNicknameException;
 import com.stubee.rollingdomains.domain.member.exception.MemberNotFoundException;
 import com.stubee.rollingdomains.domain.member.model.Member;
-import com.stubee.memberapplication.outports.LoadCurrentMemberPort;
+import com.stubee.memberapplication.outports.GetCurrentMemberPort;
 import com.stubee.rollingdomains.domain.member.model.MemberId;
 import com.stubee.rollingdomains.domain.member.services.ChangeNicknameService;
+import com.stubee.rollingdomains.domain.member.services.ElevateMemberRoleService;
 import com.stubee.rollingdomains.domain.member.services.GetMemberByIdService;
 import com.stubee.rollingdomains.domain.member.services.GetMemberInfoService;
 import com.stubee.rollingdomains.domain.member.services.commands.ChangeNicknameCommand;
@@ -19,12 +20,12 @@ import java.util.UUID;
 
 @DomainService
 @RequiredArgsConstructor
-public class MemberDomainService implements ChangeNicknameService, GetMemberByIdService, GetMemberInfoService {
+public class MemberDomainService implements ChangeNicknameService, ElevateMemberRoleService, GetMemberByIdService, GetMemberInfoService {
 
     private final CommandMemberPort commandMemberPort;
     private final QueryMemberPort queryMemberPort;
     private final CheckNicknameDuplicationPort checkNicknameDuplicationPort;
-    private final LoadCurrentMemberPort loadCurrentMemberPort;
+    private final GetCurrentMemberPort getCurrentMemberPort;
 
     @Override
     public Member getById(final UUID id) {
@@ -34,21 +35,30 @@ public class MemberDomainService implements ChangeNicknameService, GetMemberById
 
     @Override
     public Member getMember() {
-        return loadCurrentMemberPort.getMember();
+        return getCurrentMemberPort.getMember();
     }
 
     @Override
     public MemberId getMemberId() {
-        return loadCurrentMemberPort.getMemberId();
+        return getCurrentMemberPort.getMemberId();
     }
 
     @Override
     public void change(final ChangeNicknameCommand command) {
-        if(checkNicknameDuplicationPort.check(command.nickname())) {
-            throw DuplicatedNicknameException.EXCEPTION;
-        }
+        this.checkByNickname(command.nickname());
 
         commandMemberPort.saveWithId(this.getMember().changeNickname(command.nickname()));
+    }
+
+    private void checkByNickname(final String nickname) {
+        if(checkNicknameDuplicationPort.check(nickname)) {
+            throw DuplicatedNicknameException.EXCEPTION;
+        }
+    }
+
+    @Override
+    public void elevate() {
+        commandMemberPort.saveWithId(this.getMember().elevateToMember());
     }
 
 }
