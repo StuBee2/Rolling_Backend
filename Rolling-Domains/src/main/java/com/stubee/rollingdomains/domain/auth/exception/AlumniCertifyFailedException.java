4f709d0d7f17dc3ebfc686dd1b5d8d5683e0d7f4
최수원 <suwon3274@gmail.com>
2023-09-22 10:04:47 +0900

    :sparkles: :: commit 누락

---
+++ Rolling-Domains/src/main/java/com/stubee/rollingdomains/domain/auth/exception/AlumniCertifyFailedException.java
@@ -0,0 +1,14 @@
+package com.stubee.rollingdomains.domain.auth.exception;
+
+import com.stubee.rollingdomains.common.exception.CustomException;
+import com.stubee.rollingdomains.common.exception.ErrorCode;
+
+public class AlumniCertifyFailedException extends CustomException {
+
+    public static final CustomException EXCEPTION = new AlumniCertifyFailedException();
+
+    private AlumniCertifyFailedException() {
+        super(ErrorCode.ALUMNI_CERTIFY_FAILED);
+    }
+
+}
