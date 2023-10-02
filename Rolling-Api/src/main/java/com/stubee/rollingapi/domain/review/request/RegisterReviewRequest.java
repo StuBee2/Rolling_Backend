0363d3a2b6b6932d76fb3ea64ed60374dd31d35c
최수원 <suwon3274@gmail.com>
2023-10-03 00:54:48 +0900

    :bug: :: Grades min, max 설정

--- Rolling-Api/src/main/java/com/stubee/rollingapi/domain/review/request/RegisterReviewRequest.java
+++ Rolling-Api/src/main/java/com/stubee/rollingapi/domain/review/request/RegisterReviewRequest.java
@@ -3,6 +3,7 @@ package com.stubee.rollingapi.domain.review.request;
 import com.stubee.rollingdomains.domain.review.services.commands.RegisterReviewCommand;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
@@ -11,10 +12,10 @@ public record RegisterReviewRequest(
         String content,
         @NotBlank String position,
         @NotBlank String careerPath,
-        @NotNull Short salaryAndBenefits,
-        @NotNull Short workLifeBalance,
-        @NotNull Short organizationalCulture,
-        @NotNull Short careerAdvancement) {
+        @NotNull @Size(min = 1, max = 5) Short salaryAndBenefits,
+        @NotNull @Size(min = 1, max = 5) Short workLifeBalance,
+        @NotNull @Size(min = 1, max = 5) Short organizationalCulture,
+        @NotNull @Size(min = 1, max = 5) Short careerAdvancement) {
     public RegisterReviewCommand toCommand() {
         return RegisterReviewCommand.create(
                 companyId,
