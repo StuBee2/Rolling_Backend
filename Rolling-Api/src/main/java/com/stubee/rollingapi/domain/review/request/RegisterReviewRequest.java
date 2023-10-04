c20c0a1aab6c2c04d762494705a3d0a6fc0e7472
최수원 <suwon3274@gmail.com>
2023-10-04 14:05:27 +0900

    :bug: :: @Size have no validator for Short

--- Rolling-Api/src/main/java/com/stubee/rollingapi/domain/review/request/RegisterReviewRequest.java
+++ Rolling-Api/src/main/java/com/stubee/rollingapi/domain/review/request/RegisterReviewRequest.java
@@ -1,9 +1,10 @@
 package com.stubee.rollingapi.domain.review.request;
 
 import com.stubee.rollingdomains.domain.review.services.commands.RegisterReviewCommand;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
@@ -12,10 +13,10 @@ public record RegisterReviewRequest(
         String content,
         @NotBlank String position,
         @NotBlank String careerPath,
-        @NotNull @Size(min = 1, max = 5) Short salaryAndBenefits,
-        @NotNull @Size(min = 1, max = 5) Short workLifeBalance,
-        @NotNull @Size(min = 1, max = 5) Short organizationalCulture,
-        @NotNull @Size(min = 1, max = 5) Short careerAdvancement) {
+        @NotNull @Min(1) @Max(5) Short salaryAndBenefits,
+        @NotNull @Min(1) @Max(5) Short workLifeBalance,
+        @NotNull @Min(1) @Max(5) Short organizationalCulture,
+        @NotNull @Min(1) @Max(5) Short careerAdvancement) {
     public RegisterReviewCommand toCommand() {
         return RegisterReviewCommand.create(
                 companyId,
