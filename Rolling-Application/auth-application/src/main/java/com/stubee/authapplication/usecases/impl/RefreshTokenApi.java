0b204ab6ce9e3e4110982accb02b6fd250f04b73
최수원 <suwon3274@gmail.com>
2023-10-19 18:17:33 +0900

    :bug: :: Token refresh시 member 정보 최신화

--- Rolling-Application/auth-application/src/main/java/com/stubee/authapplication/usecases/impl/RefreshTokenApi.java
+++ Rolling-Application/auth-application/src/main/java/com/stubee/authapplication/usecases/impl/RefreshTokenApi.java
@@ -4,13 +4,12 @@ import com.stubee.authapplication.outports.ParseJwtPort;
 import com.stubee.authapplication.outports.ProvideJwtPort;
 import com.stubee.authapplication.usecases.RefreshTokenResponse;
 import com.stubee.authapplication.usecases.RefreshTokenUseCase;
-import com.stubee.rollingdomains.domain.member.consts.MemberRole;
+import com.stubee.rollingdomains.domain.member.model.Member;
+import com.stubee.rollingdomains.domain.member.services.GetMemberByIdService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +17,7 @@ public class RefreshTokenApi implements RefreshTokenUseCase {
 
     private final ProvideJwtPort provideJwtPort;
     private final ParseJwtPort parseJwtPort;
+    private final GetMemberByIdService getMemberByIdService;
 
     @Override
     public RefreshTokenResponse refresh(final String refreshToken) {
@@ -25,17 +25,11 @@ public class RefreshTokenApi implements RefreshTokenUseCase {
 
         final Claims claims = jwsClaims.getBody();
 
-        final String accessToken = provideJwtPort.generateAccessToken(subjectToUUID(claims), getRole(claims));
+        final Member member = getMemberByIdService.getById(Long.parseLong(claims.getSubject()));
+
+        final String accessToken = provideJwtPort.generateAccessToken(member.memberId().getId(), member.memberDetails().memberRole());
 
         return RefreshTokenResponse.of(accessToken);
-    }
-
-    private UUID subjectToUUID(Claims claims) {
-        return UUID.fromString(claims.getSubject());
-    }
-
-    private MemberRole getRole(Claims claims) {
-        return MemberRole.valueOf((String) claims.get("authority"));
     }
 
 }
