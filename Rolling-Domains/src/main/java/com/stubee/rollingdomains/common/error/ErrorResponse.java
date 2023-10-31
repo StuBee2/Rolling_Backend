ff3f7ff28892e80ad9b75450bdbfdef3795bf601
최수원 <suwon3274@gmail.com>
2023-10-31 19:57:04 +0900

    :memo: :: ErrorResponse (create -> of)

--- Rolling-Domains/src/main/java/com/stubee/rollingdomains/common/error/ErrorResponse.java
+++ Rolling-Domains/src/main/java/com/stubee/rollingdomains/common/error/ErrorResponse.java
@@ -3,7 +3,7 @@ package com.stubee.rollingdomains.common.error;
 public record ErrorResponse(
         int status,
         String message) {
-    public static ErrorResponse create(final ErrorCode error) {
+    public static ErrorResponse of(final ErrorCode error) {
         return new ErrorResponse(error.getStatusValue(), error.getMessage());
     }
 }
