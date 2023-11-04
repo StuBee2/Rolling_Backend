ab3d7a82d2fe486cc2f968f21f9b4dca7a64678c
최수원 <suwon3274@gmail.com>
2023-11-05 00:05:17 +0900

    :memo: :: code 정리

--- Rolling-Adapters/security/oauth/src/main/java/com/stubee/oauth/helper/SecurityHelper.java
+++ Rolling-Adapters/security/oauth/src/main/java/com/stubee/oauth/helper/SecurityHelper.java
@@ -3,7 +3,6 @@ package com.stubee.oauth.helper;
 import com.stubee.authapplication.outports.ParseTokenPort;
 import com.stubee.memberapplication.outports.QueryMemberPort;
 import com.stubee.oauth.model.CustomMemberDetails;
-import com.stubee.rollingdomains.domain.member.exception.MemberNotFoundException;
 import com.stubee.rollingdomains.domain.member.model.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,20 +19,29 @@ public class SecurityHelper {
     private final ParseTokenPort parseTokenPort;
     private final QueryMemberPort queryMemberPort;
 
-    public void setAuthentication(final String accessToken) {
+    public void setAuthentication(final String method, final String accessToken) {
         if(accessToken != null) {
             final Long memberId = parseTokenPort.getSubjectFromAccessToken(accessToken);
 
-            final Member member = queryMemberPort.findById(memberId)
-                    .orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+            final Member member = queryMemberPort.getById(memberId);
 
-            log.info("LoginId : {}", member.socialDetails().socialLoginId());
+            this.logLoginId(method, member.socialDetails().socialLoginId());
 
-            final CustomMemberDetails details = CustomMemberDetails.create(member);
-
-            Authentication authentication = new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
+            final Authentication authentication = getAuthentication(member);
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+    }
+
+    private Authentication getAuthentication(final Member member) {
+        final CustomMemberDetails details = CustomMemberDetails.create(member);
+
+        return new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
+    }
+
+    private void logLoginId(final String method, final String socialLoginId) {
+        switch (method) {
+            case "POST", "PATCH", "DELETE" -> log.info("{} | LoginId : {}", method, socialLoginId);
         }
     }
 
