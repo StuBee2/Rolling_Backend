2580b6c6e0a3cb3c7ebe134e3d52fd28d756f1f8
최수원 <suwon3274@gmail.com>
2023-11-05 17:51:50 +0900

    :memo: :: AsyncTransactionalEventListener 수정

--- Rolling-Application/application-commons/src/main/java/com/stubee/applicationcommons/annotations/AsyncTransactionalEventListener.java
+++ Rolling-Application/application-commons/src/main/java/com/stubee/applicationcommons/annotations/AsyncTransactionalEventListener.java
@@ -1,11 +1,14 @@
 package com.stubee.applicationcommons.annotations;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.lang.annotation.*;
 
 @Async
+@Transactional(propagation = Propagation.NEVER)
 @TransactionalEventListener
 @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
