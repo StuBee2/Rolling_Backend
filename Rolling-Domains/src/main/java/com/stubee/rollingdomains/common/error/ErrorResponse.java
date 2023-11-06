0ab780d29fee867b82e0a901d363a0a5e53d3b6e
최수원 <suwon3274@gmail.com>
2023-11-06 10:39:04 +0900

    :sparkles: :: HttpMediaTypeException handling 및 개선

--- Rolling-Domains/src/main/java/com/stubee/rollingdomains/common/error/ErrorResponse.java
+++ Rolling-Domains/src/main/java/com/stubee/rollingdomains/common/error/ErrorResponse.java
@@ -6,4 +6,8 @@ public record ErrorResponse(
     public static ErrorResponse of(final ErrorCode error) {
         return new ErrorResponse(error.getStatusValue(), error.getMessage());
     }
+
+    public static ErrorResponse of(final int status, final String message) {
+        return new ErrorResponse(status, message);
+    }
 }
