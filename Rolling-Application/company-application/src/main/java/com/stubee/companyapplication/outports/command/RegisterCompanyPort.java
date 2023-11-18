79311bed0d50ae8966a693edc7f8afba6516ab31
최수원 <suwon3274@gmail.com>
2023-11-18 19:15:45 +0900

    :art: :: 필요없는 interface 정리

--- Rolling-Application/company-application/src/main/java/com/stubee/companyapplication/outports/command/RegisterCompanyPort.java
+++
@@ -1,9 +0,0 @@
-package com.stubee.companyapplication.outports.command;
-
-import com.stubee.rollingdomains.domain.company.model.Company;
-
-public interface RegisterCompanyPort {
-
-    Company register(Company company);
-
-}
